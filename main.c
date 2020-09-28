f6fe5abae31c4a9db3dbf29eeddb1f65485d31eb
mohamad4selman <53625027+mohamad4selman@users.noreply.github.com>
2020-09-29 00:19:06 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,47 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "lib.h"
 
 int main(int argc, char *argv[]){
+    FILE *socket = fopen("socket.txt", "r");
     FILE *outputFile = fopen("table.csv", "w");
+    char line[MAXLINE];
 
-    Column *table = createTable(2);
-    setColumns(table);
+    fgets(line, sizeof(line), socket);
+    numberOfColumns = atoi(line);
+    Column *table = createTable(numberOfColumns);
+    
+    for(int i=0; i<numberOfColumns; i++){
+        fgets(line, sizeof(line), socket);
+        line[strlen(line)-1]='\0';
+        table[i].columnName = strdup(line);
+        fgets(line, sizeof(line), socket);
+        int t = atoi(line);
+        table[i].type = t;
+    }
 
-    insert(&(table[0]), "A", 1);
-    insert(&(table[0]), "A-", 2);
-    insert(&(table[0]), "B+", 3);
+    while(fgets(line, sizeof(line), socket) != NULL){
+        char *token = strtok(line, ";");
 
-    void *p;
-    int integer;
-    integer = 90; p = &integer;
-    insert(&(table[1]), p, 2);
+        if(strcmp(token,"i") == 0){
+            token = strtok(NULL, ";");
+            int row = atoi(token);
+            
+            for(int i=0; i<numberOfColumns && token != NULL; i++){
+                token = strtok(NULL, ";");
 
-    integer = 95; p = &integer;
-    insert(&(table[1]), p, 1);
+                int ti;
+                double td;
+                switch(table[i].type){
+                    case INT:
+                        ti = atoi(token);
+                        insert(&(table[i]), (void *)&ti, row);
+                        break;
+                    case FLT:
+                        td = atof(token);
+                        insert(&(table[i]), (void *)&td, row);
+                        break;
+                    case CHAR:
+                        insert(&(table[i]), (void *)(&token[0]), row);
+                        break;
+                    case STR:
+                        insert(&(table[i]), (void *)token, row);
+                        break;
+                }
+            }
+        }
+        else if(strcmp(token,"e") == 0){
+            token = strtok(NULL, ";");
+            int row = atoi(token);
+            token = strtok(NULL, ";");
+            int col = atoi(token);
+            token = strtok(NULL, ";");
 
-    integer = 60; p = &integer;
-    insert(&(table[1]), p, 3);
+            Node *cell = selectByIndex(table, row, col);
 
-    // Node *cell = selectByIndex(table, 3, 2);
-    // cell->data.i = 87;
-
-    integer = 60; p = &integer;
-    int n=0;
-    Node **x = search(table, p, INT, &n);
-    if(n>0){
-        for(int i=0; i<n; i++){
-            x[i]->data.i = 87;
+            switch(table[col-1].type){
+                case INT:
+                    cell->data.i = atoi(token);
+                    break;
+                case FLT:
+                    cell->data.f = atof(token);
+                    break;
+                case CHAR:
+                    cell->data.c = token[0];
+                    break;
+                case STR:
+                    cell->data.str = strdup(token);
+                    break;
+            }
+        }
+        else if(strcmp(token,"d") == 0){
+            token = strtok(NULL, ";");
+            int row = atoi(token);
+            token = strtok(NULL, ";");
+            int col = atoi(token);
+            delete(table, row, col);
         }
     }
 
-    //delete(table, 1, 1);
-
     printTable(table, outputFile);
-    puts("Table was printed to the file \"table.csv\"\n");
 
     freeTable(table);
     fclose(outputFile);
+    fclose(socket);
+
     return 0;
 }
