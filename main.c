99df8f823123fa6aee109a2b6e5b66362013cb5e
mohamad4selman <53625027+mohamad4selman@users.noreply.github.com>
2020-09-29 00:39:56 +0300

    Update main.c

--- main.c
+++ main.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[]){
                 token = strtok(NULL, ";");
 
                 int ti;
-                double td;
+                float td;
                 switch(table[i].type){
                     case INT:
                         ti = atoi(token);
